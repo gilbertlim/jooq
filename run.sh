7a42d699aa1be84251312931d03beee56bd71179
gilbertlim <9ilbert.app@gmail.com>
2024-06-19 15:20:22 +0900

    initialize

---
+++ run.sh
@@ -0,0 +1,2 @@
+docker volume create sakila_volume
+docker-compose up -d
---
+++ src/test/java/com/gilbert/jooq/JooqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilbert.jooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
